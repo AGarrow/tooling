f25475117bc6bc76b9fd8bc413228e0375f6ed86
Alexi Garrow <agarrow@wealthsimple.com>
2022-02-11 11:25:44 -0500

    add ccd to git aliases

--- git_aliases.sh
+++ git_aliases.sh
@@ -13,6 +13,22 @@ alias grom="git rebase origin/master"
 alias grc="git rebase --continue"
 alias gm="gc master && gpl"
 
+function ccd() {
+  curr=$(pwd)
+  target=~/ws/${1}
+  if [ -d $target ]
+  then
+    echo "repo exists locally"
+    code $target
+  else
+    cd ~/ws
+    echo "cloning " git@github.com:wealthsimple/${1}.git
+    git clone git@github.com:wealthsimple/${1}.git
+    code $target
+    cd $curr
+  fi
+}
+
 
 ## this will create a new branch on github with the same name as the current branch
 ## and open a new browser window with the pull requests page.
