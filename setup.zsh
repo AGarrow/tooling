dc7c1200c992984c858c3c5443f17e4866b83cf0
Alexi Garrow <agarrow@Alexis-MacBook-Pro.local>
2022-02-17 18:20:27 -0500

    setup slate

--- setup.zsh
+++ setup.zsh
@@ -3,4 +3,17 @@ ln -sf "$PWD/tooling.zsh" ~/tooling.zsh
 grep -qxF 'source ~/tooling.zsh' ~/.zshrc || echo 'source ~/tooling.zsh' >> ~/.zshrc
 source ~/.zshrc
 
+# config git hooks
 git config --global core.hooksPath "$PWD/.git-templates/hooks/"
+
+# config slate
+if ! test -f "/Applications/Slate.app"; then
+  echo "slate already installed"
+else
+  curr=$(pwd)
+  ln -sf "$PWD/.slate" ~/.slate
+  cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
+  open Slate.app
+  cd $curr
+fi
+
