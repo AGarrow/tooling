3b1e48e27e54418b7b8b60e1f0cd0211ddeadfa0
Alexi garrow <agarrow@getbenepass.com>
2022-10-26 09:59:19 -0400

    django aliases

---
+++ django_aliases.zsh
@@ -0,0 +1,3 @@
+alias dc="python manage.py shell"
+alias ds="python manage.py runserver"
+alias dsa="ENABLE_ADMIN=TRUE python manage.py runserver"
--- tooling.zsh
+++ tooling.zsh
@@ -3,6 +3,7 @@ autoload -Uz compinit
 compinit
 source "${DIR}/git_aliases.zsh"
 source "${DIR}/rails_aliases.zsh"
+source "${DIR}/django_aliases.zsh"
 source "${DIR}/yarn_aliases.zsh"
 source "${DIR}/wealthsimple_aliases.zsh"
 source "${DIR}/zsh_aliases.zsh"
