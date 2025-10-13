07415981215e0e6bd8981641627edb2b7c3dacd0
agarrow <agarrow@getbenepass.com>
2025-10-13 19:00:47 +0300

    still tinkering with paths

--- devcontainer-setup.zsh
+++ devcontainer-setup.zsh
@@ -1,4 +1,4 @@
 touch ~/.zshrc
-ln -sf "$PWD/tooling.zsh" ~/tooling.zsh
+ln -sf "$PWD/dotfiles/tooling.zsh" ~/tooling.zsh
 grep -qxF 'source ~/tooling.zsh' ~/.zshrc || echo 'source ~/tooling.zsh' >> ~/.zshrc
 source ~/.zshrc
